82c4b4a7aab357afe9967c9360a8d89d020915f8
bruce919 <bruce919@gmail.com>
2017-01-15 11:13:04 +0800

    修改

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,10 @@
 class CandidatesController < ApplicationController
 
+before_action :find_candidate, only: [:show, :edit, :update, :destroy] #每個方法執行前就先執行這個方法
+
 	def index
 		@candidates = Candidate.all
+		@text = "我是XXX，我當選之後，一定會。。。。。"
 	end
 
 	def new
@@ -9,15 +12,16 @@ class CandidatesController < ApplicationController
 	end
 
 	def create
-		clean_params = params.require(:candidate).permit(:name, :gender, :age, :party)
+		#確認只有這幾個資料可以進資料庫
+		# clean_params = params.require(:candidate).permit(:name, :gender, :age, :party) 
 		
-		@candidate = Candidate.new(clean_params)
+		@candidate = Candidate.new(candidate_params)
 		
 		if @candidate.save
 			flash[:notice] = "新增成功"
 			redirect_to candidates_path
 		else
-			render :new #借new.html.erb，而不是 new 的方法
+			render :new #是 new.html.erb，而不是 new 的方法
 		end
 
 	end
@@ -28,22 +32,46 @@ class CandidatesController < ApplicationController
 		if not @candidate #if not 也可寫成 unless
 			# flash[:notice] = "查無此資料" 可以把這行寫成這方式
 			redirect_to candidates_path, notice: "查無此資料"
-			#也可以全部寫成一行 redirect_to candidates_path, notice: "查無此資料" unless @candidate
+			#上面也可以全部寫成一行 redirect_to candidates_path, notice: "查無此資料" unless @candidate
 		end
 	end
 
 	def update
-		@candidate = Candidate.find_by(id: params[:id])
-		# redirect_to candidates_path, notice: "查無此資料" unless @candidates
-		
-		clean_params = params.require(:candidate).permit(:name, :gender, :age, :party)
+			# @candidate = Candidate.find_by(id: params[:id]) 底下加  private 的 find_candidate就不用寫這行
+			# redirect_to candidates_path, notice: "查無此資料" unless @candidate 底下加  private 的 find_candidate就不用寫這行
+		# find_candidate 有加 before_action 就可以不用寫這行
 
-		if @candidate.update_attributes(clean_params)
+
+		if @candidate.update_attributes(candidate_params)
 			redirect_to candidates_path, notice: "#{@candidate.name}更新成功!"
 		else
-			render :edit #是指 edit.html.erb
+			  :edit #是指 edit.html.erb
 		end
 
 	end
 
+	def destroy
+			# @candidate = Candidate.find_by(id: params[:id]) 底下加  private 的 find_candidate就不用寫這行
+			# redirect_to candidates_path, notice: "查無此資料" unless @candidate 底下加  private 的 find_candidate就不用寫這行
+		# find_candidate 有加 before_action 就可以不用寫這行
+
+		@candidate.destroy
+		redirect_to candidates_path, notice: "刪除成功！"
+	end
+
+	def show
+			# @candidate = Candidate.find_by(id: params[:id]) 底下加  private 的 find_candidate就不用寫這行
+			# redirect_to candidates_path, notice: "查無此資料" unless @candidate 底下加  private 的 find_candidate就不用寫這行
+		# find_candidate 有加 before_action 就可以不用寫這行
+	end
+
+	private
+	def find_candidate
+		@candidate = Candidate.find_by(id: params[:id])
+		redirect_to candidates_path, notice: "查無此資料" unless @candidate
+	end
+
+	def candidate_params
+		params.require(:candidate).permit(:name, :gender, :age, :party)
+	end
 end
